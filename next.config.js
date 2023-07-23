bdfabc6cbf881d1571d8744cfaf8cf33a4ed9f48
Chuci Qin <qin@yummy.ninja>
2023-07-22 20:38:36 -0400

    kanban

--- next.config.js
+++ next.config.js
@@ -1,23 +1,6 @@
-module.exports = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   reactStrictMode: true,
-  transpilePackages: [
-    "@fullcalendar/common",
-    "@babel/preset-react",
-    "@fullcalendar/common",
-    "@fullcalendar/daygrid",
-    "@fullcalendar/interaction",
-    "@fullcalendar/react",
-    "@fullcalendar/timegrid",
-    "react-github-btn",
-    "dropzone",
-  ],
-  async redirects() {
-    return [
-      {
-        source: "/",
-        destination: "/dashboards/analytics",
-        permanent: true,
-      },
-    ];
-  },
-};
+}
+
+module.exports = nextConfig
